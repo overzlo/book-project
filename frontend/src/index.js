dc8d1d71d6e44f8979874ee038a1a7fdf5cb8078
Bakytzhan Abdurakhim <bakytzhanabdurakhim@gmail.com>
2025-05-05 19:46:52 +0500

    Refactor index.js to improve code formatting and add a run function for evaluating user input

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { AuthProvider } from './context/AuthContext';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import { AuthProvider } from "./context/AuthContext"
 
+function run(userInput) {
+	return eval(userInput)
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(run("2 + 2"))
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
-);
-
+	<React.StrictMode>
+		<AuthProvider>
+			<App />
+		</AuthProvider>
+	</React.StrictMode>
+)
